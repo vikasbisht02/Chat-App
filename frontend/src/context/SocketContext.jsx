00ecf5e8928536791c0d58c4d9beba5696f7bddb
Vikas Bisht <vikasbisht732002@gmail.com>
2024-12-07 18:54:57 -0500

    socket io updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,8 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000", {
+			// const socket = io("http://localhost:8000", {
+			const socket = io("https://chat-app-w1hd.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
